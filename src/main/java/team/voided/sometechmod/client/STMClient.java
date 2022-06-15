4a2269f67a89ede22bea26b83967d7d02a3bf610
DevLocked <devlockedv1@gmail.com>
2022-06-15 19:14:06 -0400

    Almost a menu, idk what to do from here

--- src/main/java/team/voided/sometechmod/client/STMClient.java
+++ src/main/java/team/voided/sometechmod/client/STMClient.java
@@ -3,12 +3,12 @@ package team.voided.sometechmod.client;
 import net.minecraft.client.gui.screens.MenuScreens;
 import org.quiltmc.loader.api.ModContainer;
 import org.quiltmc.qsl.base.api.entrypoint.client.ClientModInitializer;
-import team.voided.sometechmod.menu.CrudeConstructingMenuAccess;
+import team.voided.sometechmod.menu.CrudeConstructingMenuScreen;
 import team.voided.sometechmod.menu.MenuRegistry;
 
 public class STMClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient(ModContainer mod) {
-		MenuScreens.register(MenuRegistry.CRUDE_CONSTRUCTING_MENU_TYPE, CrudeConstructingMenuAccess::new);
+		MenuScreens.register(MenuRegistry.CRUDE_CONSTRUCTING_MENU_TYPE, CrudeConstructingMenuScreen::new);
 	}
 }
