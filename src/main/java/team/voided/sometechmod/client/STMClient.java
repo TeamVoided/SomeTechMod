0ed8f009479b07f9e03f3e1c37444e13b2dc32ae
theendercore <theendercore@gmail.com>
2022-06-16 02:14:33 +0300

    added a thing

--- src/main/java/team/voided/sometechmod/client/STMClient.java
+++ src/main/java/team/voided/sometechmod/client/STMClient.java
@@ -1,11 +1,13 @@
 package team.voided.sometechmod.client;
 
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 import net.minecraft.client.gui.screens.MenuScreens;
 import org.quiltmc.loader.api.ModContainer;
 import org.quiltmc.qsl.base.api.entrypoint.client.ClientModInitializer;
 import team.voided.sometechmod.menu.CrudeConstructingMenuAccess;
 import team.voided.sometechmod.menu.MenuRegistry;
-
+@Environment(EnvType.CLIENT)
 public class STMClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient(ModContainer mod) {
